2d01dd4d1539f0e17d43cb987898a20a162ebfe4
tristanswire <tristan@cmdtab.co>
2016-01-12 14:59:30 -0600

    Added custom fields
    fields added to intentional page

--- page-intentional.php
+++ page-intentional.php
@@ -17,8 +17,12 @@ get_header('intentional'); ?>
 			<img src = "<?php bloginfo('template_directory'); ?>/_i/path.png" class="path">
 			<div class="intent-store">
 				<div class="wrap group">
-					<a href = "#" data-video="146207235">
-						<img src = "<?php bloginfo('template_directory'); ?>/_i/intent-header.jpg">
+					<a href = "#" data-video="<?php the_field('intent_video'); ?>">
+						<?php 
+							$intent_image = get_field('intent_header');
+							if( !empty($intent_image) ): ?>
+								<img src="<?php echo $intent_image['url']; ?>" alt="<?php echo $intent_image['alt']; ?>" />
+						<?php endif; ?>
 					</a>
 				</div>
 				<div class="med-wrap group">
@@ -169,13 +173,41 @@ get_header('intentional'); ?>
 				</div>
 				<div class="sm-wrap group">
 					<div class="quiz-form">
-						<?php gravity_form( 13, false, false, false, '', false ); ?>
+						<?php
+							$gravity = get_field('gravity_form_id');
+						?>
+						<?php gravity_form( $gravity, false, false, false, '', false ); ?>
 					</div>
 				</div><!--sm-wrap-->
 			</div><!--quiz-->
 			<div class="videos">
 				<div class="med-wrap group">
-					<ul class="video-slider bxslider">
+
+					<?php if( have_rows('video_slider') ): ?>
+
+						<ul class="video-slider bxslider">
+
+						<?php while( have_rows('video_slider') ): the_row(); 
+
+							// vars
+							$video_image = get_sub_field('video_image');
+							$video_link = get_sub_field('video_link');
+
+							?>
+
+							<li class="slide">
+								<a href = "#" data-video="<?php echo $video_link; ?>">
+									<img src="<?php echo $video_image['url']; ?>" alt="<?php echo $video_image['alt'] ?>" />
+								</a>
+							</li>
+
+						<?php endwhile; ?>
+
+						</ul>
+
+					<?php endif; ?>
+
+					<!-- <ul class="video-slider bxslider">
 						<li>
 							<a href = "#" data-video="130730908"><img src = "http://placehold.it/300x200"></a>
 						</li>
@@ -194,7 +226,7 @@ get_header('intentional'); ?>
 						<li>
 							<a href = "#" data-video=""><img src = "http://placehold.it/300x200"></a>
 						</li>
-					</ul>
+					</ul> -->
 				</div>
 			</div>
 			<div class="map-section">
@@ -205,23 +237,30 @@ get_header('intentional'); ?>
 								C182.2,156.3,157.2,181.3,126.4,181.3z M126.4,0C56.6,0,0,56.6,0,126.4c0,69.8,114.4,240.8,126.4,240.8s126.4-171,126.4-240.8
 								C252.9,56.6,196.3,0,126.4,0z"/>
 						</svg>
-						<h2>Why Intentional Parenting</h2>
-						<p>Lorem ipsum dolor sit amet, vim tibique lobortis ea, in oblique honestatis mei. Te laoreet vituperata qui. Oratio sententiae eu mea. Te sit velit assum torquatos, id zril epicurei mea. Quo munere oblique gubergren et, no sit causae euismod, ne qualisque percipitur vim</p>
+						<h2><?php the_field('why_header');?></h2>
+						<p><?php the_field('why_paragraph');?></p>
 					</header>
 					<ul class="three-list group">
 						<li>
 							<div class="info-card flip-container">
 								<div class="flipper">
 									<div class="front">
-										<img src = "<?php bloginfo('template_directory'); ?>/_i/icon-1.png">
-										<h3>Title of Card</h3>
-										<p>This is the summary of the card that is shorter than what is on the back, but the font is a little bigger.</p>
+										<?php 
+											$card_image = get_field('card_one_icon');
+											if( !empty($card_image) ): ?>
+												<img src="<?php echo $card_image['url']; ?>" alt="<?php echo $card_image['alt']; ?>" />
+										<?php endif; ?>
+										<h3><?php the_field('card_one_title'); ?></h3>
+										<p><?php the_field('card_one_text'); ?></p>
 									</div>
 									<div class="back">
-										<img src = "<?php bloginfo('template_directory'); ?>/_i/icon-black-1.png">
-										<h4>Title of Card</h4>
-										<p>Lorem ipsum dolor sit amet, vim tibique lobortis ea, in oblique honestatis mei. Te laoreet vituperata qui. Oratio sententiae eu mea.</p>
-										<p>Te sit velit assum torquatos, id zril epicurei mea. Quo munere oblique gubergren et, no sit causae euismod, ne qualisque percipitur vim</p>
+										<?php 
+											$back_card_image = get_field('card_one_back_image');
+											if( !empty($back_card_image) ): ?>
+												<img src="<?php echo $back_card_image['url']; ?>" alt="<?php echo $back_card_image['alt']; ?>" />
+										<?php endif; ?>
+										<h4><?php the_field('card_one_back_title'); ?></h4>
+										<?php the_field('card_one_back_text'); ?>
 									</div>
 								</div>
 							</div>
@@ -230,15 +269,22 @@ get_header('intentional'); ?>
 							<div class="info-card flip-container">
 								<div class="flipper">
 									<div class="front">
-										<img src = "<?php bloginfo('template_directory'); ?>/_i/icon-2.png">
-										<h3>Title of Card</h3>
-										<p>This is the summary of the card that is shorter than what is on the back, but the font is a little bigger.</p>
+										<?php 
+											$card_image = get_field('card_two_icon');
+											if( !empty($card_image) ): ?>
+												<img src="<?php echo $card_image['url']; ?>" alt="<?php echo $card_image['alt']; ?>" />
+										<?php endif; ?>
+										<h3><?php the_field('card_two_title'); ?></h3>
+										<p><?php the_field('card_two_text'); ?></p>
 									</div>
 									<div class="back">
-										<img src = "<?php bloginfo('template_directory'); ?>/_i/icon-black-2.png">
-										<h4>Title of Card</h4>
-										<p>Lorem ipsum dolor sit amet, vim tibique lobortis ea, in oblique honestatis mei. Te laoreet vituperata qui. Oratio sententiae eu mea.</p>
-										<p>Te sit velit assum torquatos, id zril epicurei mea. Quo munere oblique gubergren et, no sit causae euismod, ne qualisque percipitur vim</p>
+										<?php 
+											$back_card_image = get_field('card_two_back_image');
+											if( !empty($back_card_image) ): ?>
+												<img src="<?php echo $back_card_image['url']; ?>" alt="<?php echo $back_card_image['alt']; ?>" />
+										<?php endif; ?>
+										<h4><?php the_field('card_two_back_title'); ?></h4>
+										<?php the_field('card_two_back_text'); ?>
 									</div>
 								</div>
 							</div>
@@ -247,15 +293,22 @@ get_header('intentional'); ?>
 							<div class="info-card flip-container">
 								<div class="flipper">
 									<div class="front">
-										<img src = "<?php bloginfo('template_directory'); ?>/_i/icon-3.png">
-										<h3>Title of Card</h3>
-										<p>This is the summary of the card that is shorter than what is on the back, but the font is a little bigger.</p>
+										<?php 
+											$card_image = get_field('card_three_icon');
+											if( !empty($card_image) ): ?>
+												<img src="<?php echo $card_image['url']; ?>" alt="<?php echo $card_image['alt']; ?>" />
+										<?php endif; ?>
+										<h3><?php the_field('card_three_title'); ?></h3>
+										<p><?php the_field('card_three_text'); ?></p>
 									</div>
 									<div class="back">
-										<img src = "<?php bloginfo('template_directory'); ?>/_i/icon-black-3.png">
-										<h4>Title of Card</h4>
-										<p>Lorem ipsum dolor sit amet, vim tibique lobortis ea, in oblique honestatis mei. Te laoreet vituperata qui. Oratio sententiae eu mea.</p>
-										<p>Te sit velit assum torquatos, id zril epicurei mea. Quo munere oblique gubergren et, no sit causae euismod, ne qualisque percipitur vim</p>
+										<?php 
+											$back_card_image = get_field('card_three_back_image');
+											if( !empty($back_card_image) ): ?>
+												<img src="<?php echo $back_card_image['url']; ?>" alt="<?php echo $back_card_image['alt']; ?>" />
+										<?php endif; ?>
+										<h4><?php the_field('card_three_back_title'); ?></h4>
+										<?php the_field('card_three_back_text'); ?>
 									</div>
 								</div>
 							</div>
@@ -265,26 +318,27 @@ get_header('intentional'); ?>
 			</div><!--map-section-->
 			<div class="quote-section">
 				<div class="med-wrap group">
-					<ul class="quote-slider bxslider">
-						<li>
-							<img src = "<?php bloginfo('template_directory'); ?>/_i/intent-quote.png">
-						</li>
-						<li>
-							<img src = "http://placehold.it/1600x313">
-						</li>
-						<li>
-							<img src = "<?php bloginfo('template_directory'); ?>/_i/intent-quote.png">
-						</li>
-						<li>
-							<img src = "http://placehold.it/1600x313">
-						</li>
-						<li>
-							<img src = "<?php bloginfo('template_directory'); ?>/_i/intent-quote.png">
-						</li>
-						<li>
-							<img src = "http://placehold.it/1600x313">
-						</li>
-					</ul>
+
+					<?php if( have_rows('quote_slider') ): ?>
+
+						<ul class="quote-slider bxslider">
+
+						<?php while( have_rows('quote_slider') ): the_row(); 
+
+							// vars
+							$quote_image = get_sub_field('quote_slider_image');
+							?>
+
+							<li>
+								<img src="<?php echo $quote_image['url']; ?>" alt="<?php echo $quote_image['alt'] ?>" />
+							</li>
+
+						<?php endwhile; ?>
+
+						</ul>
+
+					<?php endif; ?>
+
 				</div>
 			</div><!--quote-section-->
 			<div class="who-section">
@@ -299,19 +353,31 @@ get_header('intentional'); ?>
 					</header>
 					<ul class="three-list group who-list">
 						<li>
-							<img src = "<?php bloginfo('template_directory'); ?>/_i/people-1.png">
-							<h3>1) Close Friends</h3>
-							<p>Te sit velit assum torquatos, id zril epicurei mea. Quo munere oblique gubergren et, no sit causae euismod, ne qualisque percipitur vim</p>
+							<?php 
+								$who_icon = get_field('who_icon_one');
+								if( !empty($who_icon) ): ?>
+									<img src="<?php echo $who_icon['url']; ?>" alt="<?php echo $who_icon['alt']; ?>" />
+							<?php endif; ?>
+							<h3><?php the_field('who_title_one'); ?></h3>
+							<p><?php the_field('who_text_one'); ?></p>
 						</li>
 						<li>
-							<img src = "<?php bloginfo('template_directory'); ?>/_i/people-2.png">
-							<h3>2) Small Group</h3>
-							<p>Te sit velit assum torquatos, id zril epicurei mea. Quo munere oblique gubergren et, no sit causae euismod, ne qualisque percipitur vim</p>
+							<?php 
+								$who_icon_two = get_field('who_icon_two');
+								if( !empty($who_icon_two) ): ?>
+									<img src="<?php echo $who_icon_two['url']; ?>" alt="<?php echo $who_icon_two['alt']; ?>" />
+							<?php endif; ?>
+							<h3><?php the_field('who_title_two'); ?></h3>
+							<p><?php the_field('who_text_two'); ?></p>
 						</li>
 						<li>
-							<img src = "<?php bloginfo('template_directory'); ?>/_i/people-3.png">
-							<h3>3) Parenting Classes</h3>
-							<p>Te sit velit assum torquatos, id zril epicurei mea. Quo munere oblique gubergren et, no sit causae euismod, ne qualisque percipitur vim</p>
+							<?php 
+								$who_icon_three = get_field('who_icon_three');
+								if( !empty($who_icon_three) ): ?>
+									<img src="<?php echo $who_icon_three['url']; ?>" alt="<?php echo $who_icon_three['alt']; ?>" />
+							<?php endif; ?>
+							<h3><?php the_field('who_title_three'); ?></h3>
+							<p><?php the_field('who_text_three'); ?></p>
 						</li>
 					</ul>
 				</div>
@@ -319,7 +385,7 @@ get_header('intentional'); ?>
 		</div><!--path-content-->
 		<div class="cta-section">
 			<div class="med-wrap group">
-				<h1>Become an Intentional Parent Today</h1><a href = "#" class="btn">Buy Now</a>
+				<h1><?php the_field('cta_header'); ?></h1><a href = "#" class="btn"><?php the_field('cta_button'); ?></a>
 			</div>
 		</div><!--cta-section-->
 		<?php while ( have_posts() ) : the_post(); ?>
