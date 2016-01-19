6fd7d83dd22dffe4aac3beeedc1fd3651130a674
Josh Rives <joshrives@gmail.com>
2016-01-19 10:45:14 -0600

    Change newsletter text

--- content-newsletter.php
+++ content-newsletter.php
@@ -8,8 +8,8 @@
 				<div class="newsletter-signup full-section">
 					<div class = "content-wrap group">
 						<span aria-hidden="true" data-icon="&#x4e;"></span>
-						<h1>Stay in Touch with HomeWord</h1>
-						<p>Subscribe to our newsletter to hear about upcoming events, special offers, and more!</p>
+						<h1>Subscribe to our Newsletters</h1>
+						<p>Stay in touch with HomeWord to hear about upcoming events, special offers, and more!</p>
 						<div class="sm-wrap">
 							<?php mc4wp_form(1160); ?>
 						</div>
